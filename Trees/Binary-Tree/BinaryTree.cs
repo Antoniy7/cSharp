fde6f4f3fe6b9d3405d88610df9902141e1a50fd
Antoniy7 <antoniy_pavlov@abv.bg>
2015-07-16 22:32:39 +0300

    c#
    c#

---
+++ Trees/Binary-Tree/BinaryTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class BinaryTree<T>
+{
+    public T value { get; set; }
+    public BinaryTree<T> LeftChild { get; set; }
+    public BinaryTree<T> RightChild { get; set; }
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        this.value = value;
+        this.RightChild = rightChild;
+        this.LeftChild = leftChild;
+    }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        Console.Write(new string(' ', 2 * indent));
+        Console.WriteLine(this.value);
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.PrintIndentedPreOrder(indent + 1);
+        }
+        if (this.RightChild != null)
+        {
+            this.RightChild.PrintIndentedPreOrder(indent + 1);
+        }
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.EachInOrder(action);
+        }
+        action(this.value);
+        if (this.RightChild != null)
+        {
+            this.RightChild.EachInOrder(action);
+        }
+
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.EachPostOrder(action);
+        }
+        if (this.RightChild != null)
+        {
+            this.RightChild.EachPostOrder(action);
+        }
+        action(this.value);
+    }
+}
---
+++ Trees/Binary-Tree/Tree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+       public T value {get; set;}
+      public IList<Tree<T>> Childer{get; private set;}
+     public Tree(T value, params Tree<T>[] children)
+     {
+         this.value=value;
+         this.Childer=new List <Tree<T>>();
+         foreach(var child in children)
+         {
+             this.Childer.Add(child);
+         }
+
+     }
+     
+      
+    public void Print(int indent = 0)
+    {
+        Console.Write(new string(' ',2*indent));
+        Console.WriteLine(this.value);
+        foreach(var child in this.Childer)
+        {
+            child.Print(indent+1);
+        }  
+    }
+
+    public void Each(Action<T> action)
+    {
+        action(this.value);
+        foreach(var child in this.Childer)
+        {
+            child.Each(action);
+        }
+    }
+
+}
+
